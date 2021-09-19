a140f9844a219a8e5aa3f846190ba76cc608289d
Lucas Henrique <lucasheriques@gmail.com>
2021-09-19 00:03:25 -0300

    Adding my own button variant

--- theme/index.ts
+++ theme/index.ts
@@ -5,6 +5,8 @@ import {
   withDefaultVariant,
 } from "@chakra-ui/react";
 
+import { mode } from "@chakra-ui/theme-tools";
+
 const inputSelectSyles = {
   variants: {
     filled: {
@@ -21,6 +23,13 @@ const inputSelectSyles = {
         borderRadius: "none",
       },
     },
+  },
+};
+
+const brandRing = {
+  _focus: {
+    ring: 2,
+    ringColor: "brand.500",
   },
 };
 
@@ -45,16 +54,29 @@ const theme = extendTheme(
       body: `Inter, ${base.fonts.body}`,
     },
     components: {
+      Button: {
+        variants: {
+          primary: (props) => ({
+            rounded: "none",
+            ...brandRing,
+            color: mode("white", "gray.800")(props),
+            backgroundColor: mode("brand.500", "brand.200")(props),
+            _hover: {
+              backgroundColor: mode("brand.600", "brand.300")(props),
+            },
+            _active: {
+              backgroundColor: mode("brand.700", "brand.400")(props),
+            },
+          }),
+        },
+      },
       Input: { ...inputSelectSyles },
       Select: { ...inputSelectSyles },
       Checkbox: {
         baseStyle: {
           control: {
             borderRadius: "none",
-            _focus: {
-              ring: 2,
-              ringColor: "brand.500",
-            },
+            ...brandRing,
           },
         },
       },
